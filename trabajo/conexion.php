7aa98b09af0f69c26597d24957ebfec23054b9a1
guillermo rodriguez <guillerhuguet@gmail.com>
2022-11-09 18:03:55 +0000

    finservidor

--- trabajo/conexion.php
+++ trabajo/conexion.php
@@ -12,10 +12,23 @@ try{
     $sql = "select nombreusu,password from credenciales";
     $registros = $bd->query($sql);
     echo "Numero de registros devueltos: " . $registros->rowCount();
+    $valido = false;
     if($registros->rowCount()>0){
+        $valido = false;
         foreach($registros as $fila){
-            
+            if ($fila["usuario"]==$nombreintroducido && password_verify($passwordintroducido,$fila["password"])){
+                //$fila["usuario"]==$nombreintroducido && $fila["password"]==$passwordintroducido
+                $valido = true;
+                
+            }
         }
+        if($valido){
+            echo "<h1>correcto</h1>";
+        }else{
+            echo "<h1>Invalido</h1>";
+        }
+    }else{
+        echo "<h1>Esta vacio</h1>";
     }
    // echo "Numero de registros devueltos: " . $registros->rowCount();
     //echo $registros;
