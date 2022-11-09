d27cabb5ac1d4817a7674a5187b7305bec4165aa
guillermo rodriguez <guillerhuguet@gmail.com>
2022-11-09 16:31:31 +0000

    comi

--- public/index.php
+++ public/index.php
@@ -1,5 +1,2 @@
 <?php
-
-echo "<h2>Contenido en public</h2>";
-
 require "../start.php";
--- start.php
+++ start.php
@@ -1,2 +1,17 @@
 <?php
+/**
+ * RECURSO/ACCION/PARAMETRO
+ * recurso: controladores
+ * accion: metodos del controlador
+ * parametros: find  id del producto
+ */
 echo "<h2>Contenido privado</h2>";
+require "../Controller.php";
+/*$controller = new Controller();
+$controller->home();
+echo "SHOW";
+$controller->show();*/
+if(isset($_GET["method"])){
+    $method->GET["method"];//show
+    $app->method();
+}
