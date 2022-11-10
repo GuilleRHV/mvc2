41bf31882cb251098254bccb8f2bd1f9cd3777da
guillermo rodriguez <guillerhuguet@gmail.com>
2022-11-10 16:15:52 +0000

    llk

---
+++ app/controllers/ProductController.php
@@ -0,0 +1,29 @@
+<?php
+
+class ProductController{
+
+    function __construct()
+    {
+        echo "<br>Constructor clase PRODUCTCONTROLLER";
+
+    }//fin_constructor
+
+
+    function index(){
+        echo "<br>Dentro index de PRODUCTCONTROLLER";
+        //metodo home de Controller de mvc00
+        $products = Product::all();
+        require "views/home.php";
+    }//fin_mindex
+
+    function show(){
+        echo "<br>Dentro show de PRODUCTCONTROLLER";
+        //metodo show de Controller de mvc00
+        $id = $_GET["id"];
+        $product = Product::find($id);
+        require "views/show.php";
+    }//fin_mindex
+
+
+
+}
