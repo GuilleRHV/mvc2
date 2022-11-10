b4a6d4f7846ea9d6ff9e348a73d752206c30c17c
guille <guillerhuguet@gmail.com>
2022-11-10 10:03:52 +0000

    avanz

--- public/trabajo/views/valido.php
+++ public/trabajo/views/valido.php
@@ -1,11 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <h1>Es valido</h1>
+    <form action="" method="post">
+        <label for="">Crear contacto</label>
+
+        <p><input type="button" name="crear" value="Crear"></p>
+        <label for="">Eliminar contacto</label>
+        <p><input type="button" name="eliminar" value="Eliminar"></p>
+    </form>
 </body>
+
 </html>
