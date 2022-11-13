c741bd6e5bd7f03487cae4073389181a4feb96d0
guille <guillerhuguet@gmail.com>
2022-11-13 16:36:50 +0000

    avanz

---
+++ public/trabajo/trabajobien/views/eliminarusuario.php
@@ -0,0 +1,22 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Eliminar</title>
+</head>
+
+<body>
+    <h1>Eliminar usuario</h1>
+    <form action="?method=valido" method="post">
+        <label for="">Nombre de la persona que quieres eliminar</label>
+        <p><input type="text" name="nombreeliminar"></p>
+        
+        <input type="submit" name="envioeliminar" value="eliminar usuario">
+    </form>
+
+</body>
+
+</html>
