a32f46212bd1f617bea284a22db5b85c89693bba
guillermo rodriguez <guillerhuguet@gmail.com>
2022-11-17 15:44:08 +0000

    ll

--- public/preparestatement.php
+++ public/preparestatement.php
@@ -13,19 +13,37 @@ $db = new PDO($dsn,$usuario,$password);
 //Establece el nivel de error que muestra la excepcion
 $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 
-$sentencia = $db->prepare("INSERT INTO credenciales (nombreusu,password) VALUES (:nombre,:clave)");
+
+
+//prepare por nombre
+//$sentencia = $db->prepare("INSERT INTO credenciales (nombreusu,password) VALUES (:nombre,:clave)");
+
+
+//preparacion por posicion
+$sentencia = $db->prepare("INSERT INTO credenciales (nombreusu,password) VALUES (?,?)");
+$nombre = "Alicia";
+$clave1 = "sombrero";
+$sentencia->bindParam(1,$nombre);
+$sentencia->bindParam(2,$clave1);
+
+///////////
 //Paso 2
+/*
 $nombre = "Juan";
 $clave1 = "1234";
 $sentencia->bindParam(':nombre',$nombre);
 $sentencia->bindParam('clave',$clave1);
+*/
+//preparacion + ejecucion 
+//$stmt ->execute(array(':nombre'=> $nombre, ':ciudad'=>$ciudad)
+
 
 //Saldra Juan, no Pedro
 //$sentencia->bindValue(':nombre',$nombre);
 //$sentencia->bindValue('clave',$clave1);
 
-$nombre = "Pedro";
-$clave1 = "789";
+//$nombre = "Pedro";
+//$clave1 = "789";
 $sentencia->execute(); //ejecuta la sentencia
 
 echo "<h2>Exito</h2>";
