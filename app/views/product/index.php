4d4c1c6d34fdf5e0051ca3992b67c313abb6f693
guillermo rodriguez <guillerhuguet@gmail.com>
2022-11-24 16:20:11 +0000

    findwes

---
+++ app/views/product/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Lista de productos</h1>
+
+    <table class="table table-striped table-hover">
+        <tr>
+            <th>Nombre</th>
+            <th>Precio</th>
+            <th>F. compra</th>
+            <th></th>
+        </tr>
+
+        <?php foreach ($products as $key => $producto) { ?>
+            <tr>
+                <td><?php echo $producto->name ?></td>
+             
+                <td><?php echo $producto->price ?></td>
+                <td><?php echo $producto->fecha_compra ?></td>
+                <td>
+                    <a href="/product/show/<?php echo $producto->id ?>" class="btn btn-primary">Ver </a>
+                </td>
+            </tr>
+        <?php } ?>
+        
+    </table>
+</body>
+
+</html>
